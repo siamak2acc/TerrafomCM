eaec511404b50e938fc4042e43c97ddd282a743a
Sia Fahim <sia.fahim@bizit.local>
2023-10-31 20:20:11 +1100

    cahnge subname

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "811-cbfc47a6-provide-continuous-delivery-with-gith1"
+  name     = "811-cbfc47a6-provide-continuous-delivery-with-gith"
   location = "eastus"
 }
 
